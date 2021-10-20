25a184dba3e6e9c1c52dd6a25332b925d279eb2b
ChatoJimenez <jesgui99@gmail.com>
2021-10-20 15:07:32 -0500

    Cambio de los ResponseEntities del ArrayController

--- src/main/java/com/example/springproject/web/ArrayController.java
+++ src/main/java/com/example/springproject/web/ArrayController.java
@@ -1,14 +1,11 @@
 package com.example.springproject.web;
 
 import com.example.springproject.model.Array;
-import com.example.springproject.model.Message;
 import com.example.springproject.service.ArrayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @RestController
@@ -23,19 +20,19 @@ public class ArrayController {
     }
 
     @GetMapping("/duplicates/{id}")
-    public ResponseEntity<String> getDuplicates(@PathVariable int id){
+    public ResponseEntity<?> getDuplicates(@PathVariable int id){
         return  ResponseEntity.ok("Id: "+ arrayService.getArrayById(id).getId() +"\nName: "+ arrayService.getArrayById(id).getName()
                 +"\nData: "+arrayService.getAll(id) + "\nDuplicates: " + arrayService.getDuplicates(id));
     }
 
     @GetMapping("/largest/{id}")
-    public ResponseEntity<String> getLargest(@PathVariable int id){
+    public ResponseEntity<?> getLargest(@PathVariable int id){
         return  ResponseEntity.ok("Id: "+ arrayService.getArrayById(id).getId() +"\nName: "+ arrayService.getArrayById(id).getName()
                 +"\nData: "+arrayService.getAll(id)+ "\nLargest element: " + arrayService.getLargestElement(id));
     }
 
     @GetMapping("/first/{id}")
-    public ResponseEntity<String> getFirst(@PathVariable int id){
+    public ResponseEntity<?> getFirst(@PathVariable int id){
         return  ResponseEntity.ok("Id: "+ arrayService.getArrayById(id).getId() +"\nName: "+ arrayService.getArrayById(id).getName()
                 +"\nData: "+arrayService.getAll(id)+ "\nFirst element: " + arrayService.getFirstElement(id));
     }
@@ -52,36 +49,21 @@ public class ArrayController {
     }
 
     @PostMapping("/arrays")
-    public ResponseEntity<Message> createArray(@RequestBody Array arr) {
+    public ResponseEntity<?> createArray(@RequestBody Array arr) {
         arrayService.createArray(arr);
-        return new ResponseEntity<>(new Message(
-                "The array with id: " + arr.getId() +" was created successfully",
-                HttpStatus.CREATED.value(),
-                HttpStatus.CREATED,
-                ZonedDateTime.now()),
-                HttpStatus.CREATED);
+        return new ResponseEntity<>("The array with id: " + arr.getId() +" was created successfully", HttpStatus.CREATED);
     }
 
     @PutMapping("/arrays/{id}")
-    public ResponseEntity<Message> updateArray(@PathVariable int id, @RequestBody Array arr){
+    public ResponseEntity<?> updateArray(@PathVariable int id, @RequestBody Array arr){
         arrayService.updateArray(id, arr);
-        return new ResponseEntity<>(new Message(
-                "The array with id: " + id + " was updated successfully",
-                HttpStatus.OK.value(),
-                HttpStatus.OK,
-                ZonedDateTime.now()),
-                HttpStatus.OK);
+        return new ResponseEntity<>("The array with id: " + id + " was updated successfully", HttpStatus.OK);
     }
 
     @DeleteMapping("/arrays/{id}")
-    public ResponseEntity<Message> deleteArray(@PathVariable int id) {
+    public ResponseEntity<?> deleteArray(@PathVariable int id) {
         arrayService.deleteArray(id);
-        return new ResponseEntity<>(new Message(
-                "The array with id: " + id + " was deleted successfully",
-                HttpStatus.OK.value(),
-                HttpStatus.OK,
-                ZonedDateTime.now()),
-                HttpStatus.OK);
+        return new ResponseEntity<>("The array with id: " + id + " was deleted successfully", HttpStatus.OK);
     }
 
 }
